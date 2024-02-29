d168c1440f197d6aa1b4b067a41a37adcd08e149
senmer5 <132789897+senmer5@users.noreply.github.com>
2024-02-29 17:11:45 +0100

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Title of the application
+st.title("Araba Tahmin Uygulaması")
+
+# Header
+st.header("Arabanızın Fiyatını Tahmin Edin")
+
+# Subheader
+st.subheader("Bu uygulamayı kullanarak arabınızın değerini öğrenin.")
